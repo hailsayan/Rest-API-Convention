08a46161cee32f3403bc2a7c71826f2053483063
Psyon <143930298+hailsayan@users.noreply.github.com>
2024-01-22 23:34:56 +0330

    error middleware

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -9,6 +9,12 @@ const getContacts = (req, res)=>{
 //@route POST /api/contacts
 //@access public
 const createContact = (req, res)=>{
+    console.log(req.body);
+    const { name, email, phone } = req.body;
+    if(!name || !email || !phone){
+        res.status(400);
+        throw new Error('all fields are mandatory')
+    }
     res.status(201).json({ message : 'create contact' });
 };
 
